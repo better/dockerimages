6a7ba8023b8019e04e9d07b510d728a55347a53f
Ben Connito <bconnito@better.com>
2020-02-15 15:27:48 -0500

    use sort -V for correct numerical

--- semversort.sh
+++ semversort.sh
@@ -3,10 +3,10 @@
 version_weight () {
   echo -e "$1" | tr ' ' "\n"  | \
     sed -Ee 's:^([0-9]+)\.([0-9]+)\.([0-9]+)$:&:' | \
-    sed -Ee 's:^([0-9]+)\.([0-9]+)$:&.x:' | \
-    sed -Ee 's:^([0-9]+)$:&.x.x:'
+    sed -Ee 's:^([0-9]+)\.([0-9]+)$:&.99999999:' | \
+    sed -Ee 's:^([0-9]+)$:&.99999999.99999999:'
 }
 tags_orig=($(cat))
 tags_weight=( $(version_weight "${tags_orig[*]}") )
 
-for y in ${tags_weight[*]}; do echo ${y}; done | sort -r | sed -Ee 's:\.x::g'
+for y in ${tags_weight[*]}; do echo ${y}; done | sort -Vr | sed -Ee 's:\.99999999::g'
