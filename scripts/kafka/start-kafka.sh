e6223a2b8aff4b33a7519e222511e14ebf521e56
fluffywaffles <skorlir@gmail.com>
2020-03-24 12:19:23 -0400

    Feat: kafka: get advertised.listeners from environment
    This should allow us to actually network together containers in docker
    compose by setting the hostname in the Kafka cluster configuration to
    match the hostname of the container.

--- scripts/kafka/start-kafka.sh
+++ scripts/kafka/start-kafka.sh
@@ -3,4 +3,6 @@ until bash $ZOOKEEPER_HOME/bin/zkServer.sh status; do
   echo "Zookeeper not yet started"
   sleep 0.5
 done
-exec bash $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+exec bash $KAFKA_HOME/bin/kafka-server-start.sh \
+  $KAFKA_HOME/config/server.properties \
+  --override advertised.listeners=${ADVERTISED_LISTENERS}
