8240007a61bd24d722539c9c03bb12f4e6b8b587
Ethan Langevin <ethan@better.com>
2020-06-05 23:32:18 -0400

    Allow customizing all settings for multiple listeners
    This allows this image to work in docker and on the host network at the same time.
    Also bumped up the versions slightly because the image wouldn't build

--- scripts/kafka/start-kafka.sh
+++ scripts/kafka/start-kafka.sh
@@ -6,3 +6,5 @@ done
 exec bash $KAFKA_HOME/bin/kafka-server-start.sh \
   $KAFKA_HOME/config/server.properties \
   --override advertised.listeners=${ADVERTISED_LISTENERS}
+  --override listeners=${LISTENERS}
+  --override listener.security.protocol.map=${LISTENER_SECURITY_PROTOCOL_MAP}
