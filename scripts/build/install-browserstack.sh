93a607f023a683ac4eff63a52b22d02f56c2f795
Tim Duffy <tduffy@better.com>
2020-09-24 16:45:45 -0400

    Add additional package deps

--- scripts/build/install-browserstack.sh
+++ scripts/build/install-browserstack.sh
@@ -5,7 +5,7 @@
 
 GLIBC_VERSION=glibc-2.32-r0.apk
 
-apk add libstdc++6
+apk add --no-cache libstdc++ libc6-compat
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub &&
   wget "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.32-r0/$GLIBC_VERSION" &&
