bd23d8741dbcca2d64e5bea5380c79058fa0b543
Tim Duffy <tduffy@better.com>
2020-09-24 19:39:07 -0400

    Try using alpine version

--- scripts/build/install-browserstack.sh
+++ scripts/build/install-browserstack.sh
@@ -5,14 +5,14 @@
 
 GLIBC_VERSION=2.32-r0
 
-apk add --no-cache libstdc++6
+apk add --no-cache libstdc++ procps
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub &&
   wget "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/$GLIBC_VERSION/glibc-$GLIBC_VERSION.apk" &&
   apk add "glibc-$GLIBC_VERSION.apk" &&
   rm "glibc-$GLIBC_VERSION.apk"
 
-wget -q https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip &&
+wget -q https://www.browserstack.com/browserstack-local/BrowserStackLocal-alpine.zip &&
   unzip BrowserStackLocal-linux-x64.zip &&
   chmod +x BrowserStackLocal &&
   mkdir -p /root/.browserstack/ &&
