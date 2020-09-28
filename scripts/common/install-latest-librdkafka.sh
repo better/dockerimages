befa5aa2284d5ec557ad9db97d1b309ce38d0a80
fluffywaffles <skorlir@gmail.com>
2020-09-28 17:55:46 -0400

    Refactor/Major: standardize python on 3.7.9 debian buster (slim)
    Upgrade moves us from a loosely-enforced standard python version of
    3.7.3 to exactly 3.7.9 across (almost) all python images. 3.7.3 could
    not be used because we wanted a python base image on 3.7.x with debian
    upgraded from stretch (end-of-life 9.x) to buster (latest stable 10.x).
    Decided to bite the bullet and jump to the latest 3.7.x patch instead of
    trying to stay close to 3.7.3. Should be fine, if semver was followed.
    
    Python version is only fixed at 3.7.9 across build and base python
    images. Other images may intentionally choose a different version. (This
    means legacy images, and "run" images that are used to run commands for
    development tasks, CI, et cetera.)
    
    scripts/common/install-latest-librdkafka.sh is upgraded to use apt-get
    instead of apk. Due to differences of capability between apk and
    apt-get, some features are changed or removed. (Notably, removal of
    build dependencies has to be handled with environment variables.)
    
    base/dataengineering: largely unchanged; python 3.7.3 → 3.7.9
    base/python         : alpine → debian 10 slim; python → 3.7.9
    base/python-postgres: alpine → debian 10; python → 3.7.x (no control)
    build/python        : alpine → debian 10 slim; python → 3.7.9
    build/python.tox    : pinned to build-python-1; needs manual refactor
    
    Deviations:
    
    python-postgres is not pinned to 3.7.9. We install python into the
    docker-library/postgres image using apt, which does not give us control
    over the patch version of python. This is acceptable, though not ideal.
    
    Problems:
    
    python.tox is not moved to debian. Use of tox, pyenv, and pipx may cause
    unexpected issues. Need to confer with @tokuchan to find an appropriate
    plan of attack for upgrading without causing breakage.
    
    python.tox WILL NOT BUILD until it is refactored so that it is either
    made independent of build/python and upgraded to debian buster slim, or
    otherwise refactored so it can `FROM` the new debian-based build/python
    image, because `install-latest-librdkafka` has been upgraded to use
    `apt-get` instead of `apk` for managing build dependencies.

--- scripts/common/install-latest-librdkafka.sh
+++ scripts/common/install-latest-librdkafka.sh
@@ -6,18 +6,21 @@ kafka_version=1.5.0
 path__kafka_tarfile="v${kafka_version}.tar.gz"
 path__kafka_build_directory="librdkafka-${kafka_version}"
 
+apt_install="apt-get install --no-install-recommends --yes"
+apt_purge="apt-get purge --yes"
+temporary_dependencies=(curl make gcc g++)
+purge_temporary="${PURGE_BUILD_DEPENDS}"
+
 ### Install librdkafka
 ## Install user dependencies
-apk add --no-cache libressl-dev
-apk add --no-cache cyrus-sasl-dev
-apk add --no-cache zstd-dev
-apk add --no-cache zlib-dev
+${apt_install} \
+  zlib1g-dev   \
+  libssl-dev   \
+  libzstd-dev  \
+  libsasl2-dev
 
 ## Install developer dependencies
-apk add -t kafka-installer --no-cache curl
-apk add -t kafka-installer --no-cache make
-apk add -t kafka-installer --no-cache gcc
-apk add -t kafka-installer --no-cache g++
+${apt_install} ${temporary_dependencies[*]}
 
 ## Fetch librdkafka from github
 curl -LO "https://github.com/edenhill/librdkafka/archive/v${kafka_version}.tar.gz"
@@ -35,7 +38,9 @@ rm -f "${path__kafka_tarfile}"
 rm -rf "${path__kafka_build_directory}"
 
 ## Install the confluent kafka python library
-pip install confluent-kafka==${kafka_version}
+python3 -m pip install confluent-kafka==${kafka_version}
 
 ## Remove the temporary packages we needed to build with
-apk del kafka-installer
+if [[ -n "${purge_temporary}" ]]; then
+  ${apt_purge} ${temporary_dependencies[*]}
+fi
